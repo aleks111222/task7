3527327a3b4099a8198c30127d1ff92d613f4c58
aleks <aleksander.rebisz@gmail.com>
2022-06-15 22:19:54 +0200

    task7 ver 1

--- task7.py
+++ task7.py
@@ -1,17 +1,29 @@
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import *
+from sklearn.metrics import cohen_kappa_score, classification_report
 import pandas as pd
-from pgmpy.models import BayesianModel
-from pgmpy.estimators import MaximumLikelihoodEstimator
-from pgmpy.inference import VariableElimination
 
-xData = pd.read_csv("Testing.csv")
+data = pd.read_csv("Training.csv")
+X_train = data[data.columns[:-1]]
+y_train = data[data.columns[-1]]
 
-print(xData.columns.size())
+data = pd.read_csv("Testing.csv")
+X_test = data[data.columns[:-1]]
+y_test = data[data.columns[-1]]
 
-model = BayesianModel([('itching', 'skin_rash'), ('itching', 'joint_pain'), ('joint_pain', 'scurring'), ('scurring', 'prognosis')])
+types_of_models = [ ("gaussian", GaussianNB()),
+                    ("multinomial", MultinomialNB()),
+                    ("complement", ComplementNB()), 
+                    ("bernoulli", BernoulliNB()), 
+                    ("categorical", CategoricalNB())]
 
-model.fit(xData, estimator = MaximumLikelihoodEstimator)
+for model_type in types_of_models:
+    model_name = model_type[0]
+    model = model_type[1]
 
-infer = VariableElimination(model)
-q = infer.query(variables = ['prognosis'], evidence = {'itching' : 1, 'cough' : 1})
+    model.fit(X_train, y_train)
+    predicted = model.predict(X_test)
 
-print(q)
+    print(f"Predicted labels:\n{predicted}")
+    print(f"Classification report for {model_name}:\n{classification_report(predicted, y_test)}")
+    print(cohen_kappa_score(predicted, y_test))
